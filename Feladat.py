677873cae68d0ef43b5592c12d8afff53e9a145c
Roli0410 <muller.gabor.roland@diak.szbi-pg.hu>
2025-11-05 11:30:23 +0100

    Roli Dani

--- Feladat.py
+++ Feladat.py
@@ -24,38 +24,56 @@ error_message_invalid_username = "The username cannot start with a '.' character
 ok_message = "Valid email address :)"
 is_valid = True
 
+hibak = 0
+
 while is_valid:
     if number_of_at_characters == 0:
         print(error_message_no_at)
+        hibak += 1
 
     elif number_of_at_characters > 1:
         print(error_message_too_many_at)
+        hibak += 1
 
     else:
         username, domain = email.split("@")
+        hibak += 1
 
         if len(username) == 0:
             print(error_message_no_username)
+            hibak += 1
 
         if username[0] == ".":
             print(error_message_invalid_username)
+            hibak += 1
 
         if len(domain) == 0:
             print(error_message_no_domain)
+            hibak += 1
+
         else:
             if "." not in domain:
                 print(error_message_no_dot_in_domain)
+                hibak += 1
             else:
                 domain_parts = domain.split(".")
                 if len(domain_parts[0]) == 0:
                     print(error_message_no_server_name)
+                    hibak += 1
+
                 if len(domain_parts[-1]) == 0:
                     print(error_message_no_tld)
+                    hibak += 1
+
                 elif len(domain_parts[-1]) < 2:
                     print(error_message_short_tld)
+                    hibak += 1
 
     if number_of_dot_characters == 0:
         print(error_message_no_dot)
-    
+        hibak += 1
+
     break
 
+if hibak == 0:
+    print(ok_message)
